a8d5648ae206345be552df18b5942301e0b70a6b
lewanmat <56128830+lewanmat@users.noreply.github.com>
2025-03-31 14:52:41 +0200

    Add files via upload - initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDetails(projectId) {
+    const projectDetails = document.getElementById(projectId);
+    
+    if (projectDetails.style.display === "block") {
+        projectDetails.style.display = "none";
+    } else {
+        projectDetails.style.display = "block";
+    }
+}
